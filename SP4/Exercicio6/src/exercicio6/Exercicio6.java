74c91a7eedaf50b9d5566bd60b8a42e74ec41a70
Unknown <andrelisenuneslemos@gmail.com>
2018-10-14 20:46:11 -0300

    Pré atualização

---
+++ SP4/Exercicio6/src/exercicio6/Exercicio6.java
@@ -0,0 +1,42 @@
+/*
+Faça um programa que crie uma pilha de números de ponto flutuante usando a classe Stack. O
+programa deve exibir o seguinte menu:
+1 – Inserir item no topo
+2 – Retirar item do topo
+3 - Sair
+Digite sua opção: __
+Na opção (1) acima o programa deve solicitar que o usuário digite um número (use o tratamento de
+exceções do Java para validar a entrada). Na opção (2) o item retirado da pilha deve ser mostrado na tela.
+Trate todas as exceções que possam vir a ocorrer.
+ */
+package exercicio6;
+
+import java.util.Stack;
+import java.util.EmptyStackException;
+
+public class Exercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Stack<Number> stk = new Stack<Number>();
+//stk.push(12L); stk.push(34567); stk.push(1.0F);
+//try {
+//Number n = null;
+//while (true) {
+//n = stk.pop();
+//System.out.println("Popped " + n);
+//}
+//} catch (EmptyStackException e) {
+//e.printStackTrace();
+    }
+    
+}
+
+//
+//public class StackTest {
+//public static void main(String[] args) {
+
+//} } }
